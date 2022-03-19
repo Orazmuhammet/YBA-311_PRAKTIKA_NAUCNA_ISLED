13c671cec851e8d3042829e8afc8dcd293ad6004
Oraz <82648244+Orazmuhammet@users.noreply.github.com>
2022-03-20 01:30:15 +0300

    Add files via upload

---
+++ LibraryDemo/LibraryDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="Program.cs" company="Васильева М.А.">
+// Copyright (c) Васильева М.А.. All rights reserved.
+// </copyright>
+
+namespace LibraryDemo
+{
+    using System;
+    using Domain;
+    using ORM;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        private static void Main()
+        {
+            var brand = new Brand(1, "lada vesta", "Россия");
+
+            var car = new Car(1, 1999, "синий", 600000, brand);
+
+            var worker = new Worker(1, "Лёвин И. Д.", "Продавец");
+
+            var carsale = new Carsale(1, DateTime.Now, 650000, car, worker);
+
+            Console.WriteLine($"{car} --> {brand}");
+
+            Console.WriteLine($"{carsale} --> {car}");
+
+            Console.WriteLine($"{carsale} --> {worker}");
+
+            using var sessionFactory = NHibernateConfigurator.GetSessionFactory(showSql: true);
+            using var session = sessionFactory.OpenSession();
+            session.Save(brand);
+            session.Save(car);
+            session.Save(carsale);
+            session.Save(worker);
+            session.Flush();
+        }
+    }
+}
