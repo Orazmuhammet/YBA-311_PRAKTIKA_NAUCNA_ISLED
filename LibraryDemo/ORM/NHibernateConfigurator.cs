e94d97b6983f48bc549d717f9e4f5e11d89178ce
Oraz <82648244+Orazmuhammet@users.noreply.github.com>
2022-03-20 11:02:30 +0300

    Add files via upload

---
+++ LibraryDemo/ORM/NHibernateConfigurator.cs
@@ -0,0 +1,69 @@
+﻿// <copyright file="NHibernateConfigurator.cs" company="Васильева М.А.">
+// Copyright (c) Васильева М.А.. All rights reserved.
+// </copyright>
+
+namespace ORM
+{
+    using System.Reflection;
+    using FluentNHibernate.Cfg;
+    using FluentNHibernate.Cfg.Db;
+    using NHibernate;
+    using NHibernate.Cfg;
+    using NHibernate.Tool.hbm2ddl;
+
+    /// <summary>
+    /// Класс для настройки NHibernate.
+    /// </summary>
+    public static class NHibernateConfigurator
+    {
+        /// <summary>
+        /// Конфигурация.
+        /// </summary>
+        private static FluentConfiguration fluentConfiguration;
+
+        /// <summary>
+        /// Фабрика сессий (<see cref = "ISessionFactory"/>).
+        /// </summary>
+        /// <param name="assembly"> Целевая сборка. </param>
+        /// <param name="showSql"> Показывать генерируемый SQL-код. </param>
+        /// <returns> Фабрику сессий. </returns>
+        public static ISessionFactory GetSessionFactory(Assembly assembly = null, bool showSql = false)
+        {
+            return GetConfiguration(assembly ?? Assembly.GetExecutingAssembly(), showSql)
+                .BuildSessionFactory();
+        }
+
+        private static FluentConfiguration GetConfiguration(Assembly assembly = null, bool showSql = false)
+        {
+            if (fluentConfiguration is null)
+            {
+                var databaseConfiguration = MsSqlConfiguration.MsSql2012.ConnectionString(
+                    x => x
+                        .Server(@"LAPTOP-1QBD3V4H\SQLEXPRESS")
+                        .Database("DealershipBD")
+                        .TrustedConnection());
+
+                if (showSql)
+                {
+                    databaseConfiguration = databaseConfiguration.ShowSql().FormatSql();
+                }
+
+                fluentConfiguration = Fluently.Configure()
+                    .Database(databaseConfiguration)
+                    .Mappings(m => m.FluentMappings.AddFromAssembly(assembly))
+                    .ExposeConfiguration(BuildSchema);
+            }
+
+            return fluentConfiguration;
+        }
+
+        /// <summary>
+        /// Метод, который создает таблицы, если их не было в схеме по конфигурации.
+        /// </summary>
+        /// <param name="configuration"> Конфигурация ORM, которая содержит правила отображения.</param>
+        private static void BuildSchema(Configuration configuration)
+        {
+            new SchemaExport(configuration).Execute(true, true, false);
+        }
+    }
+}
